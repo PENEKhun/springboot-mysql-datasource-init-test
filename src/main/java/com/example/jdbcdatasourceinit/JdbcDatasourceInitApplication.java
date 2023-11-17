46295438fd24641431d893602bf54bea0b09e045
penekhun <penekhun@gmail.com>
2023-11-17 19:20:24 +0900

    Initial commit

---
+++ src/main/java/com/example/jdbcdatasourceinit/JdbcDatasourceInitApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcdatasourceinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDatasourceInitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JdbcDatasourceInitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jdbcdatasourceinit/JdbcDatasourceInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcdatasourceinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDatasourceInitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
